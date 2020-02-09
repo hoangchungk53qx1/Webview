8b18eafac14f31e3c00d3d32c3c7e05c1d90ae27
hoangchung136 <hoangchungk53qx12gmail.com>
2020-02-09 14:58:29 +0700

    add my files

---
+++ src/main/java/com/bhsoft/news/MainActivity.java
@@ -0,0 +1,115 @@
+package com.bhsoft.news;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView superImageView;
+    ProgressBar superProgressBar;
+    WebView superWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intview();
+        superProgressBar.setMax(100);
+        superWebView.loadUrl("https://donghuongthanhhoa.vn");
+        WebSettings webSettings = superWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        superWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+        superWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                superProgressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                superImageView.setImageBitmap(icon);
+
+            }
+        });
+
+        superWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Uri myUri = Uri.parse(url);
+                Intent superIntent = new Intent(Intent.ACTION_VIEW);
+                superIntent.setData(myUri);
+                startActivity(superIntent);
+            }
+        });
+    }
+
+    private void intview() {
+        superImageView = findViewById(R.id.myImageView);
+        superProgressBar = findViewById(R.id.myProgressBar);
+        superWebView = findViewById(R.id.myWebView);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (superWebView.canGoBack()) {
+            superWebView.goBack();
+        } else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle("Thoát");
+            dialog.setMessage("Bạn có muốn thoát ! ");
+            dialog.setPositiveButton("Thoát", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            dialog.setCancelable(false);
+            dialog.setNegativeButton("ở lại", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+
+        }
+    }
+
+}
